4385351f51fa0ac50478857c7b69af783feb1d11
Daniel Campoverde [alx741] <alx@sillybytes.net>
2017-04-28 13:52:57 -0500

    Add usage printing function

--- nsresgen.sh
+++ nsresgen.sh
@@ -10,3 +10,10 @@ AND_lDPI="36x36"
 IOS_3X="192x192"
 IOS_2X="96x96"
 IOS_1X="48x48"
+
+function printUsage
+{
+    echo "NativeScript Resource Generator"
+    echo "Usage:  nsresgen [FILE]"
+    # echo "This command must be invoked from within the root of a NS project"
+}
